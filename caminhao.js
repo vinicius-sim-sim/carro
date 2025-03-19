3ba114c4225af19166dc77a53cd89491a937a3c3
vinicius-sim-sim <vinicius.concolato@gmail.com>
2025-03-19 08:47:01 -0300

    Add files via upload

---
+++ caminhao.js
@@ -0,0 +1,25 @@
+class Caminhao extends Carro {
+    constructor(modelo, cor, capacidadeCarga) {
+      super(modelo, cor);
+      this.capacidadeCarga = capacidadeCarga;
+      this.cargaAtual = 0;
+    }
+  
+    carregar(peso) {
+      if (this.cargaAtual + peso <= this.capacidadeCarga) {
+        this.cargaAtual += peso;
+        console.log(`Caminhão carregado. Carga atual: ${this.cargaAtual} kg`);
+      } else {
+        console.log("Carga excede a capacidade do caminhão.");
+      }
+    }
+  
+    descarregar(peso) {
+       if (this.cargaAtual - peso >= 0) {
+          this.cargaAtual -= peso;
+          console.log(`Caminhão descarregado. Carga atual: ${this.cargaAtual} kg`);
+       } else {
+           console.log("Não é possível descarregar mais do que a carga atual.");
+       }
+    }
+  }
---
+++ esportivo.js
@@ -0,0 +1,21 @@
+class CarroEsportivo extends Carro {
+    constructor(modelo, cor) {
+      super(modelo, cor); // Chama o construtor da classe pai (Carro)
+      this.turboAtivado = false;
+    }
+  
+    ativarTurbo() {
+      if (this.ligado) {
+        this.turboAtivado = true;
+        this.acelerar(50); // Acelera mais rápido com o turbo
+        console.log("Turbo ativado!");
+      } else {
+        console.log("O carro precisa estar ligado para ativar o turbo.");
+      }
+    }
+  
+    desativarTurbo() {
+      this.turboAtivado = false;
+      console.log("Turbo desativado!");
+    }
+  }
